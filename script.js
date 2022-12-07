5c8b24b0cf9c765ae46822c3981b3b97bf658eb6
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-07 14:50:17 +0530

    added click to expand and close to button

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 window.onload = function () {
-	var then = new Date("2022-12-31").getTime();
-	var x = setInterval(function () {
-		var now = new Date().getTime();
-		var distance = then - now;
+	var card = document.getElementById("card");
 
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor(
-			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
-		);
-		var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	card.onclick = function () {
+		if (card.classList.contains("card-click")) {
+			card.classList.remove("card-click");
+		} else {
+			card.classList.add("card-click");
+			var then = new Date("2022-12-31").getTime();
+			var x = setInterval(function () {
+				var now = new Date().getTime();
+				var distance = then - now;
 
-		document.getElementById("card").innerHTML =
-			days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
+				var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+				var hours = Math.floor(
+					(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
+				);
+				var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById("card").innerHTML = "EXPIRED";
+				document.getElementById("card").innerHTML =
+					days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
+
+				if (distance < 0) {
+					clearInterval(x);
+					document.getElementById("card").innerHTML = "EXPIRED";
+				}
+			}, 1000);
 		}
-	}, 1000);
+	};
 };
