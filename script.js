a5a7d910b2864a38de6cbe4ff3b043e1f3f87771
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-07 16:33:03 +0530

    multiple event listner added, need to fix the innerHTML change

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-function setDate() {
-	var then = new Date("2022-12-31").getTime();
-	var now = new Date().getTime();
-	var distance = then - now;
-
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor(
-		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
-	);
-	var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-	card.innerHTML = days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
-
-	if (distance < 0) {
+function clickEvent() {
+	if (this.classList.contains("card-click")) {
+		this.classList.remove("card-click");
+		this.innerHTML =
+			this.classList[0].charAt(0).toUpperCase() + this.classList[0].slice(1);
 		clearInterval(x);
-		card.innerHTML = "EXPIRED";
+	} else {
+		this.classList.add("card-click");
+		x = setInterval(function () {
+			var then = new Date("2022-12-25").getTime();
+			var now = new Date().getTime();
+			var distance = then - now;
+
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor(
+				(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
+			);
+			var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			this.innerHTML =
+				days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
+
+			if (distance < 0) {
+				clearInterval(x);
+				this.innerHTML = "EXPIRED";
+			}
+		}, 1000);
 	}
 }
 window.onload = function () {
-	var card = document.getElementById("card");
+	var cards = document.getElementsByClassName("card");
 	var x = null;
-	card.onclick = function () {
-		if (card.classList.contains("card-click")) {
-			card.classList.remove("card-click");
-			card.innerHTML = "Christmas";
-			clearInterval(x);
-		} else {
-			card.classList.add("card-click");
-			x = setInterval(setDate, 1000);
-		}
-	};
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].onclick = clickEvent;
+	}
 };
