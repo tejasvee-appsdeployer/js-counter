225c4f61c501ffd3e1787169c8739d20e0016dd6
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-07 15:13:20 +0530

    fixed name change after click

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+function setDate() {
+	var then = new Date("2022-12-31").getTime();
+	var now = new Date().getTime();
+	var distance = then - now;
+
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
+	);
+	var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	card.innerHTML = days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
+
+	if (distance < 0) {
+		clearInterval(x);
+		card.innerHTML = "EXPIRED";
+	}
+}
 window.onload = function () {
 	var card = document.getElementById("card");
-
+	var x = null;
 	card.onclick = function () {
 		if (card.classList.contains("card-click")) {
 			card.classList.remove("card-click");
+			card.innerHTML = "Christmas";
+			clearInterval(x);
 		} else {
 			card.classList.add("card-click");
-			var then = new Date("2022-12-31").getTime();
-			var x = setInterval(function () {
-				var now = new Date().getTime();
-				var distance = then - now;
-
-				var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-				var hours = Math.floor(
-					(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
-				);
-				var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-				var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-				document.getElementById("card").innerHTML =
-					days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
-
-				if (distance < 0) {
-					clearInterval(x);
-					document.getElementById("card").innerHTML = "EXPIRED";
-				}
-			}, 1000);
+			x = setInterval(setDate, 1000);
 		}
 	};
 };
