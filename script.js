b51fd95dbb5f47a782ea64cf324c1c05e885687c
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-07 14:31:04 +0530

    made simple js counter and card div

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+	var then = new Date("2022-12-31").getTime();
+	var x = setInterval(function () {
+		var now = new Date().getTime();
+		var distance = then - now;
+
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24)
+		);
+		var mintues = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		document.getElementById("card").innerHTML =
+			days + "d " + hours + "h " + mintues + "m " + seconds + "s ";
+
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("card").innerHTML = "EXPIRED";
+		}
+	}, 1000);
+};
